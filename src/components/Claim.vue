0c5d743ba9fa240d0549622510560e20c7e2178c
maxiu1g <maxymiliantoja@gmail.com>
2019-09-21 18:16:12 +0200

    Add basic hero image view

---
+++ src/components/Claim.vue
@@ -0,0 +1,36 @@
+<template>
+<div clas="claimWrapper">
+   <h1 class="claim">SPACER</h1>
+  <p clas="subclaim">
+    Begin your journey throught our amazing galaxy,
+    and discover places you never even heard of.
+  </p>
+  <p clas="subclaim">
+    Type anything spcae-related to stard.
+  </p>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'Claim',
+};
+</script>
+
+<style lang="scss" scoped>
+
+  .claimWrapper{
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+  .claim{
+    font-size: 50px;
+  }
+
+  .subclaim{
+    margin-top: 20px 0 0 0;
+    font-size: 13px;
+  }
+</style>
--- src/router.js
+++ src/router.js
@@ -1,6 +1,5 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import About from './views/About.vue';
 import Search from './views/Search.vue';
 
 Vue.use(Router);
@@ -11,14 +10,6 @@ export default new Router({
       path: '/',
       name: 'Search',
       component: Search,
-    },
-    {
-      path: '/about',
-      name: 'About',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: About,
     },
   ],
 });
