678180984d347d8cdc34ac2fd883758a307d0ae3
maxiu1g <maxymiliantoja@gmail.com>
2019-09-22 15:46:18 +0200

    Finished app

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,173 @@
 <template>
-  <div class="app">
-    <router-view></router-view>
+  <div :class="[{ flexStart: step === 1 }, 'wrapper']">
+    <transition name="slide">
+      <img src="./assets/logo.svg" class="logo" v-if="step === 1">
+    </transition>
+    <transition name="fade">
+      <Heroimage v-if="step === 0" />
+    </transition>
+    <Claim v-if="step === 0" />
+    <Searchinput v-model="searchValue" @input="handleInput" :dark="step ===1" />
+    <div class="results" v-if="results && !loading && step=== 1">
+      <Item v-for="item in results" :item="item" :key="item.data[0].nasa_id" @click.native="handleModalOpen(item)" />
+    </div>
+    <div class="loader" v-if="step===1 && loading" />
+    <Modal v-if="modalOpen" :item="modalItem" @closeModal="modalOpen = false"/>
   </div>
 </template>
+<script>
+import axios from 'axios';
+import debounce from 'lodash.debounce';
+import Claim from '@/components/Claim.vue';
+import Searchinput from '@/components/Searchinput.vue';
+import Heroimage from '@/components/Heroimage.vue';
+import Item from '@/components/Item.vue';
+import Modal from '@/components/Modal.vue';
+
+const API = 'https://images-api.nasa.gov/search';
+export default {
+  name: 'Search',
+  components: {
+    Modal,
+    Heroimage,
+    Item,
+    Claim,
+    Searchinput,
+  },
+  data() {
+    return {
+      modalOpen: false,
+      modalItem: null,
+      loading: false,
+      step: 0,
+      searchValue: '',
+      results: [],
+    };
+  },
+  methods: {
+    handleModalOpen(item) {
+      this.modalOpen = true;
+      this.modalItem = item;
+    },
+    handleInput: debounce(function () {
+      this.loading = true;
+      console.log(this.searchValue);
+      axios.get(`${API}?q=${this.searchValue}&media_type=image`)
+        .then((response) => {
+          this.results = response.data.collection.items;
+          this.loading = false;
+          this.step = 1;
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }, 500),
+  },
+};
+</script>
 
 <style lang="scss">
   @import url('https://fonts.googleapis.com/css?family=Montserrat:300i,400,600,800&display=swap');
   *{
     box-sizing: border-box;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
   }
 
-  body{
+  body {
     font-family: 'Montserrat', sans-serif;
     margin: 0;
     padding: 0;
+  }
+
+  .fade-enter-active, .fade-leave-active {
+    transition: opacity .3s ease;
+  }
+
+  .fade-enter, .fade-leave-to {
+    opacity: 0;
+  }
+
+  .slide-enter-active, .slide-leave-active {
+    transition: margin-top .8s ease;
+  }
+
+  .slide-enter, .slide-leave-to {
+    margin-top: -50px;
+  }
+
+  .wrapper{
+    margin: 0;
+    position: relative;
+    width: 100%;
+    min-height: 100vh;
+    height: 100vh;
+    padding: 30px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+
+    &.flexStart{
+      justify-content: flex-start;
+    }
+  }
+
+  .loader {
+    margin-top: 100px;
+    display: inline-block;
+    position: relative;
+    width: 64px;
+    height: 64px;
+    @media (min-width: 768px) {
+      width: 90px;
+      height: 90px;
+    }
+  }
+  .loader:after {
+    content: " ";
+    display: block;
+    border-radius: 50%;
+    width: 0;
+    height: 0;
+    margin: 6px;
+    box-sizing: border-box;
+    border: 26px solid #1e3d4a;
+    border-color: #1e3d4a transparent #1e3d4a transparent;
+    animation: loading 1.2s infinite;
+    @media (min-width: 768px) {
+      width: 90px;
+      height: 90px;
+    }
+  }
+  @keyframes loading {
+    0% {
+      transform: rotate(0);
+      animation-timing-function: cubic-bezier(0.55, 0.055, 0.675, 0.19);
+    }
+    50% {
+      transform: rotate(900deg);
+      animation-timing-function: cubic-bezier(0.215, 0.61, 0.355, 1);
+    }
+    100% {
+      transform: rotate(1800deg);
+    }
+  }
+
+
+  .logo{
+    position: absolute;
+    top: 40px;
+  }
+
+  .results {
+    margin-top: 50px;
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    grid-gap: 20px;
+
+    @media (min-width: 768px) {
+      grid-template-columns: 1fr 1fr 1fr ;
+    }
   }
 </style>
--- src/components/Claim.vue
+++ src/components/Claim.vue
@@ -1,11 +1,11 @@
 <template>
-<div clas="claimWrapper">
+<div class="claimWrapper">
    <h1 class="claim">SPACER</h1>
-  <p clas="subclaim">
+  <p class="subclaim">
     Begin your journey throught our amazing galaxy,
     and discover places you never even heard of.
   </p>
-  <p clas="subclaim">
+  <p class="subclaim">
     Type anything spcae-related to stard.
   </p>
 </div>
@@ -21,16 +21,34 @@ export default {
 
   .claimWrapper{
     display: flex;
+    text-align: center;
     flex-direction: column;
     justify-content: center;
     align-items: center;
+    color: white;
   }
   .claim{
     font-size: 50px;
+    margin: 0;
+
+    @media (min-width: 768px) {
+      font-size: 80px;
+    }
+    @media (min-width: 1024px) {
+      font-size: 100px;
+    }
   }
 
   .subclaim{
     margin-top: 20px 0 0 0;
     font-size: 13px;
+    font-weight: 400;
+
+    @media (min-width: 768px) {
+      font-size: 17px;
+      width: 500px;
+      line-height: 1;
+      letter-spacing: 1px;
+    }
   }
 </style>
